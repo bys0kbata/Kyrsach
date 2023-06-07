e7c64fdbfa2594924925945c1f9a874f5466a0c7
bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2023-06-07 23:12:37 +0400

    Update nameTable.java

--- src/main/java/com/example/kyrsach/Metod/nameTable.java
+++ src/main/java/com/example/kyrsach/Metod/nameTable.java
@@ -1,15 +1,11 @@
 package com.example.kyrsach.Metod;
-
-
 import java.util.Date;
-
 public class nameTable {
         private String nameFile;
         private String sizeFile;
         private Date lastOperationFile;
         private String typeFile;
         private String puth;
-
         public nameTable( String nameFile, String sizeFile,Date lastOperationFile,String typeFile,String puth) {
             this.nameFile = nameFile.substring(nameFile.lastIndexOf("/")+1);
             this.sizeFile = sizeFile;
@@ -24,26 +20,21 @@ public class nameTable {
         public void setNameFile(String nameFile) {
             this.nameFile = nameFile;
         }
-
         public String getSizeFile() {
             return sizeFile;
         }
-
         public void setSizeFile(String sizeFile) {
             this.sizeFile = sizeFile;
         }
-
         public Date getLastOperationFile() {
             return lastOperationFile;
         }
-
         public void setLastOperationFile(Date lastOperationFile) {
             this.lastOperationFile = lastOperationFile;
         }
     public String getTypeFile() {
         return typeFile;
     }
-
     public void setTypeFile(String typeFile) {
         this.typeFile = typeFile;
     }
